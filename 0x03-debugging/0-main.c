5b246b84c068867dbe59a5787a0052f1dba1e0b6
BOUTAINA <boutainarahmoni3@gmail.com>
2023-06-22 08:21:48 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main ~ tests function that prints it integer is positive or negative
+ * return 0
+ */
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
